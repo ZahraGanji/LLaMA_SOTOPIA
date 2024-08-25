768b1a892667cf260989aeb4ace7868750b06f5f
FreddieHorn <fryderykrog7@gmail.com>
2024-08-25 20:29:56 +0200

    added a pipeline

--- evaluation.py
+++ evaluation.py
@@ -7,7 +7,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 # source ~/NLP/venv/bin/activate
 #  source ~/localdisk/pythonvenv/bin/activate
-def llama_3_prompt_creation(description):
+def llama_3_judge_prompt_creation(description):
 
     json_format = {
         "explanation": {"Why do you think given movie description is real or not"},
@@ -39,37 +39,18 @@ def llama_3_prompt_creation(description):
     
     return(messages)
 def evaluate_scene_v2(description, model, tokenizer, json_schema):
-    # messages = [
-    # {"role": "system", "content": """You are a movie expert. You know every movie scene by heart. Your job is to check whether the input movie scene description is real or not.\
-    #  If it's real - value of 'is_real' will be set to true, if it's not, it will be set to 'false'. Answer best to your movie knowledge."""},
-    # {"role": "user", "content": f"{description}"},
-    # ]
-    messages = llama_3_prompt_creation(description)
+    messages = llama_3_judge_prompt_creation(description)
     prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True, return_tensors="pt")
     jsonformer = Jsonformer(model, tokenizer, json_schema, prompt, max_number_tokens=500,
                         max_array_length=500,
                         max_string_token_length=500)
-    # prompt = "Generate a person's information based on the following schema:"
     generated_data = jsonformer()
     return generated_data
-# pipeline1 = pipeline(
-#     "text-generation",
-#     model=model_name,
-#     model_kwargs={"torch_dtype": torch.bfloat16})
-# pipeline2 = pipeline(
-#     "text-generation",
-#     model=model_name,
-#     model_kwargs={"torch_dtype": torch.bfloat16})
-# pipeline3 = pipeline(
-#     "text-generation",
-#     model=model_name,
-#     model_kwargs={"torch_dtype": torch.bfloat16})
+
 agent1 = AutoModelForCausalLM.from_pretrained(model_name)
 agent2 = AutoModelForCausalLM.from_pretrained(model_name)
 agent3 = AutoModelForCausalLM.from_pretrained(model_name)
-# Create a pipeline for text classification
-
-# evaluated_scene = "Two thugs are counting their loot after a robbery on a Gotham City rooftop. Suddenly, they hear a noise and look around nervously. Thug 1 says, ‘What was that?’ Thug 2 replies, ‘Probably just a rat. Let’s get out of here.’ Before they can move, a dark figure swoops down and lands in front of them. It’s Batman. Thug 1 asks, ‘Who are you?’ Batman grabs Thug 1 by the collar and lifts him off the ground, saying, ‘I’m Batman.’ Batman knocks out both thugs effortlessly and ties them up for the police. He then disappears into the night, leaving only the sound of his cape flapping in the wind."
+valuated_scene = "Two thugs are counting their loot after a robbery on a Gotham City rooftop. Suddenly, they hear a noise and look around nervously. Thug 1 says, ‘What was that?’ Thug 2 replies, ‘Probably just a rat. Let’s get out of here.’ Before they can move, a dark figure swoops down and lands in front of them. It’s Batman. Thug 1 asks, ‘Who are you?’ Batman grabs Thug 1 by the collar and lifts him off the ground, saying, ‘I’m Batman.’ Batman knocks out both thugs effortlessly and ties them up for the police. He then disappears into the night, leaving only the sound of his cape flapping in the wind."
 evaluated_scene = "King Jumbo bumbo goes out on a marika island and handles out blue coconuts. Then a conflict arises because on of the island residents did not receive enough coconuts. The negotiation lasts for 3 hours before the kind kills all the island residents"
 json_schema = {
             "type": "object",
--- pipeline.py
+++ pipeline.py
@@ -21,14 +21,29 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
 
     # generate the movie scenario
-    movie_scenario = generate_response(prompt)
+    movie_scenario = generate_movie_scene(prompt)
 
     # evaluate whether the scenario is real or made up
     scenario_judges = [AutoModelForCausalLM.from_pretrained(MODEL_NAME) for _ in range(config["judges_no"])]
-
+    json_schema_evaluation = {
+            "type": "object",
+            "properties": {
+                "is_real": {"type": "boolean"},
+                "explanation": {"type": "string"}
+                }
+        }
     yes_count = 0
     for i in range(judges_no):
         anwser = evaluate_scene_v2(movie_scenario, scenario_judges[i], tokenizer)
+        if anwser["is_real"]:
+            yes_count+=1
+    
+    if yes_count >= 2:
+        print("Generated movie scenario exists according to Llama-3. Proceeding to the next step")
+    else:
+        print("Generated movie scenario does not exist according to Llama-3")
+
+
 
     
 
